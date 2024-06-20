ca529e9e7d0c17a7a4acfa3a46dce6f6086fb0e0
ElGarkarGit <idv22c.sgranados@uartesdigitales.edu.mx>
2024-06-20 11:39:26 -0600

    Funcionalidad de esfera, resorte.
    Se puede prácticamente copiar la funcionalidad de la esfera para el enemigo, con su simple tag se puede determinar si es enemigo o esfera del jugador.

---
+++ Assets/Garkar/Scripts/SpringScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringScript : MonoBehaviour
+{
+    [SerializeField] private float m_springForce;
+    public float SpringForce { get { return m_springForce; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
