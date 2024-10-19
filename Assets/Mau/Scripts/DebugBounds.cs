14dd294b4cfb099b9c04333d79536932bb22f554
MautronGG <mautron03@gmail.com>
2024-10-19 13:57:25 -0600

    debugbounds

--- Assets/Mau/Scripts/DebugBounds.cs
+++ Assets/Mau/Scripts/DebugBounds.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class DebugBounds : MonoBehaviour
+public partial class DebugBounds : MonoBehaviour
 {
     Collider2D m_Collider;
     public Vector3 m_Center;
     public Vector3 m_Size, m_Min, m_Max, m_Extents;
     void Start()
+    {
+        Bounds();
+    }
+    public void Bounds()
     {
         m_Collider = GetComponent<Collider2D>();
         m_Center = m_Collider.bounds.center;
@@ -15,5 +20,20 @@ public class DebugBounds : MonoBehaviour
         m_Min = m_Collider.bounds.min;
         m_Max = m_Collider.bounds.max;
         m_Extents = m_Collider.bounds.extents;
+        
+    }
+}
+
+[CustomEditor(typeof(DebugBounds))]
+public partial class DebugBoundsEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        DebugBounds db = (DebugBounds)target;
+        if (GUILayout.Button("Get Bounds"))
+        {
+            db.Bounds();
+        }
     }
 }
